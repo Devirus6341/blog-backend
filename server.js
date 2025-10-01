1c98fdf2a6ad6a52f637a34cb232e3cf774de50d
Devirus6341 <devirus101@gmail.com>
2025-10-01 09:34:09 +0100

    updated cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 dotenv.config();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://blog-frontend-gxz6.onrender.com',
   credentials: true
 }));
 app.use(express.json());
